7f71b86f79b871c5ce5d6ab6980617ca5f5ee797
Joe Loeffler <johannes.r.loeffler@gmail.com>
2025-02-24 16:06:20 -0800

    Initial Commit

---
+++ Concentrator/ConcentratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentratorApp.swift
+//  Concentrator
+//
+//  Created by Johannes Löffler on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcentratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcentratorTests/ConcentratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentratorTests.swift
+//  ConcentratorTests
+//
+//  Created by Johannes Löffler on 2/24/25.
+//
+
+import Testing
+@testable import Concentrator
+
+struct ConcentratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
